02b21da9f1c9ee3d92b3e9af262801325ca2614e
rishit banker <rishitbanker314@gmail.com>
2026-07-25 14:09:04 +0530

    Fix socket.io client connecting to localhost in production

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,7 +4,7 @@
  */
 import { io } from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = import.meta.env.PROD ? '/' : 'http://localhost:3001';
 
 const socket = io(SERVER_URL, {
   autoConnect: true,
